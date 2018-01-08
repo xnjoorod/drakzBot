e0f49716b248ba4bed4b37b9f440c4b18ae59aa5
João Rodrigues <joaoadriano3@gmail.com>
2018-01-08 01:44:36 +0000

    Temporary fix on JSON pre-fetch getting null

--- twitch.js
+++ twitch.js
@@ -12,17 +12,19 @@ function checkTwitchStreams(streamers, channel, liveStatus) {
             let result = JSON.parse(body)
             if(result.stream !== null) {
               if(liveStatus[name] == 0) {
-                const embed = new Discord.RichEmbed()
-                  .setTitle("O streamer " + result.stream.channel.display_name + " acabou de entrar em direto na Twitch!")
-                  .setDescription("Acompanha já a transmissão em direto!")
-                  .setThumbnail(result.stream.preview.small)
-                  .addField("Título", result.stream.channel.status, true)
-                  .addField("Viewers: ", result.stream.viewers, true)
-                  .setURL("https://www.twitch.tv/" + result.stream.channel.display_name)
-                  .setColor("#6034b1")
-                  .setFooter('João Rodrigues © 2018');
-                channel.send({ embed });
-                liveStatus[name] = 1;
+                if(result.stream.channel.display_name !== null) {
+                  const embed = new Discord.RichEmbed()
+                    .setTitle("O streamer " + result.stream.channel.display_name + " acabou de entrar em direto na Twitch!")
+                    .setDescription("Acompanha já a transmissão em direto!")
+                    .setThumbnail(result.stream.preview.small)
+                    .addField("Título", result.stream.channel.status, true)
+                    .addField("Viewers: ", result.stream.viewers, true)
+                    .setURL("https://www.twitch.tv/" + result.stream.channel.display_name)
+                    .setColor("#6034b1")
+                    .setFooter('João Rodrigues © 2018');
+                  channel.send({ embed });
+                  liveStatus[name] = 1;
+                }
               } else {
                 //todo: remove message if user sets offline
               }
