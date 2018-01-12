e5de9c9682f3b3992bfbebc5b46d5bd7ac25807d
João Rodrigues <joaoadriano3@gmail.com>
2018-01-12 22:27:00 +0000

    Removed old not working code from commands

--- commands/website.js
+++ commands/website.js
@@ -1,8 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-    var msg = message.content.toLowerCase();
-    if (msg === 'd!website' || msg === 'd!site') {
-        message.reply('pode encontrar o website do drakz em http://www.drakz.pt !');
-    }
+    message.reply('pode encontrar o website do drakz em http://www.drakz.pt !');
 }
 
 module.exports.config = {
