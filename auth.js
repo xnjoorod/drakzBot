cf85a23160681054539f55c7b60ebcf1219b81c3
Filipe Almeida <filipe.lmd@gmail.com>
2018-01-17 21:26:29 +0000

    Auth now checks if auth.json exists as it is not available on production environment.

--- auth.js
+++ auth.js
@@ -1,12 +1,26 @@
-const auth = require('./auth.json');
+
+const fs = require('fs')
+
 
 module.exports = () => {
-
     const isProduction = process.env.hasOwnProperty('BOT_TOKEN')
-        && process.env.hasOwnProperty('TWITCH_CLIENT_ID')
+    && process.env.hasOwnProperty('TWITCH_CLIENT_ID')
+    const authExists = fs.existsSync('./auth.json')
+    
+    var token, twitchClientID
+    if (isProduction) {
+        token = process.env.BOT_TOKEN
+        twitchClientID = process.env.TWITCH_CLIENT_ID
+    } else if (authExists) {
+        const auth = require('./auth.json')
+        token = auth.token
+        twitchClientID = auth.twitch_clientId
+    } else {
+        throw Exception('Could not find auth credentials')
+    }
 
     return {
-        token: isProduction ? process.env.BOT_TOKEN : auth.token,
-        twitch_clientId: isProduction ? process.env.TWITCH_CLIENT_ID : auth.twitch_clientId
+        token: token,
+        twitch_clientId: twitchClientID
     }
 }
